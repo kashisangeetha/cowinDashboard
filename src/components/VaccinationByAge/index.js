96a40470dd2293e934ff320120db681a541cdb73
sangeetha <sangeetha@gamail.com>
2024-04-30 07:46:11 +0000

    cowinDashboard

---
+++ src/components/VaccinationByAge/index.js
@@ -0,0 +1,36 @@
+// Write your code here
+import {PieChart, Pie, Legend, Cell} from 'recharts'
+
+import './index.css'
+
+const VaccinationByAge = props => {
+  const {vaccinationByAgeDetails} = props
+
+  return (
+    <div className="vaccination">
+      <h1>Vaccination by Age</h1>
+      <PieChart width={1000} height={300}>
+        <Pie
+          data={vaccinationByAgeDetails}
+          cx="50%"
+          cy="30%"
+          outerRadius="60%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#2d87bb" />
+          <Cell name="44-60" fill="#a3df9f" />
+          <Cell name="Above 60" fill="#64C2A6" />
+        </Pie>
+
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+          wrapperStyle={{fontsize: 12, fontFamily: 'Roboto'}}
+        />
+      </PieChart>
+    </div>
+  )
+}
+export default VaccinationByAge
